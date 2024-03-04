2ab32040bfcb31e577c1fd46f3192d0952f25e3e
Henrique Medeiros <84346920+henriquemcmedeiros@users.noreply.github.com>
2024-03-04 13:38:20 +0000

    unicafe exercises done

---
+++ Part1/unicafe/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.header}</h1>
+    </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </>
+  );
+};
+
+const Statistics = ({ good, neutral, bad}) => {
+  const total = good + neutral + bad
+
+  if (total > 0) {
+    return (
+      <>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="total" value={total} />
+            <StatisticLine text="average" value={(good - bad) / total} />
+            <StatisticLine text="positive" value={good * 100 / total + "%"} />
+          </tbody>
+        </table>
+      </>
+    );
+  }
+  else {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <>
+      <Header header={"give feedback"} />
+      <Button text={"good"} onClick={handleGoodClick} />
+      <Button text={"neutral"} onClick={handleNeutralClick} />
+      <Button text={"bad"} onClick={handleBadClick} />
+
+      <Header header={"statistics"} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </>
+  )
+}
+
+export default App
